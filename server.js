04ae1aa14148e6f594d3cfc9f6f5090e4ea52b6c
Ben F <56423163+bferriman@users.noreply.github.com>
2021-05-09 14:49:53 -0600

    added logic to add games to database when received on POST route

--- server.js
+++ server.js
@@ -15,10 +15,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const databaseUrl = "MERN-practice";
+const collections = ["games"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+})
+
 app.post("/api/games", (req, res) => {
   const newGame = req.body;
   console.log(newGame);
-  res.json(newGame);
+  db.games.insert(newGame, (error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
